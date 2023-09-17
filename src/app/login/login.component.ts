4c1040975daa5c67243f9d52237739b383a472b3
“Natejjp” <nathanjjpham@gmail.com>
2023-09-17 10:06:40 -0400

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,11 +14,12 @@ export class LoginComponent {
 
   login() {
     // Replace 'yourUsername' and 'yourPassword' with the desired username and password
-    const correctUsername = 'yourUsername';
-    const correctPassword = 'yourPassword';
+    const correctUsername = 's40169089';
+    const correctPassword = '9089WG18';
 
     if (this.username === correctUsername && this.password === correctPassword) {
       this.loggedIn = true;
+      this.attemptedLogin = false;
       this.message = 'Good job Westin!';
       setTimeout(() => {
         this.loggedIn = false; // Reset login state after a delay
