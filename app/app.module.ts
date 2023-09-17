ca42d378322fd2f0b550fb8ce0d380d278920efa
“Natejjp” <nathanjjpham@gmail.com>
2023-09-17 10:42:29 -0400

    please

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import { LoginComponent } from './login/login.component';
-import { ConfettiComponent } from './confetti/confetti.component';
+import { LoginComponent } from '../login/login.component';
+import { ConfettiComponent } from '../confetti/confetti.component';
 
 @NgModule({
   declarations: [
